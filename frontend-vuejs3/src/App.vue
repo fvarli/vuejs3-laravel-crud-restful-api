ffcf603460435da06e82d207d1ba942a32250dff
Ferzender Varli <fvarlii@gmail.com>
2022-04-27 03:00:12 +0300

    first commit

---
+++ frontend-vuejs3/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <Header title="Frontend Vuejs3" />
+    <router-view></router-view>
+</template>
+
+<script>
+import Header from './components/Header';
+export default {
+  name: 'App',
+  components: {
+      Header
+  }
+}
+</script>
+
+<style>
+
+</style>
